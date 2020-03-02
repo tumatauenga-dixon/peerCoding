b0652c18ec3d6e05a67e9006f97eb203a92524f5
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-03 09:54:12 +1300

    added sum

---
+++ index2.js
@@ -0,0 +1,16 @@
+var numOne=(40);
+var numTwo=(50);
+var numThree=(60);
+var numFour=(70);
+
+var sumOne=(numOne / numTwo);
+var sumTwo=(numThree / numOne);
+var sumThree=(numFour % numFour);
+
+var sumFour=(sumOne + sumTwo - sumThree);
+
+console.log("sum = " + sumFour);
+
+
+
+// numOne, / numTwo, + numThree, / numOne, - numFour, % numFour
