3db811ee2ce0b52bd558d46464634f6875e3e78e
josh <josh-moss@live.nmit.ac.nz>
2020-03-03 09:39:37 +1300

    Add two numbers

---
+++ index.js
@@ -0,0 +1,17 @@
+var numOne;
+var numTwo;
+var Sum;
+
+
+numOne = (600);
+numTwo = (300);
+
+sum = (numOne + numTwo)
+
+console.log (numOne + numTwo)
+console.log (numOne - numTwo)
+console.log (numOne % numTwo)
+console.log (numOne * numTwo)
+console.log (numOne / numTwo)
+console.log ("sum = " + sum)
+//
