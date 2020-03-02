c5ec09cf06df91befb2ec194677baa1f5a2a5a42
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-03 09:46:18 +1300

    added four variables

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var numTwo;
 
 numOne=(40);
 numTwo=(30);
+plus=(numOne + numTwo);
+minus=(numOne - numTwo);
+div=(numOne / numTwo);
+percent=(numOne % numTwo);
+multi=(numOne * numTwo);
 
-console.log(numOne + numTwo);
-console.log(numOne - numTwo);
-console.log(numOne / numTwo);
-console.log(numOne % numTwo);
-console.log(numOne * numTwo);
+console.log("sum = " + plus);
+console.log("sum = " + minus);
+console.log("sum = " + div);
+console.log("sum = " + percent);
+console.log("sum = " + multi);
 
 //this is a comment
 /* This is also a comment */
