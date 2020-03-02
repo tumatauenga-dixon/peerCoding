98d204e27874754b56464aac7040774f01603bea
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-03 10:39:40 +1300

    math

--- math.js
+++ math.js
@@ -1,15 +1,9 @@
-var numOne=(40);
-var numTwo=(50);
-var numThree=(60);
-var numFour=(70);
+var numOne=40;
+var numTwo=50;
+var numThree=60;
+var numFour=70;
 
-var sumOne=(numOne / numTwo);
-var sumTwo=(numThree / numOne);
-var sumThree=(numFour % numFour);
-
-var sum=(sumOne + sumTwo - sumThree);
-
-console.log("sum = " + sum);
+console.log("sum = " + ((numOne / numTwo)+(numThree / numOne)-(numFour % numFour)));
 
 
 
