1dfccee653b168346f423eb7ea5f7def43a0f05c
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-03 09:20:15 +1300

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+var numOne;
+var numTwo;
+
+numOne=(40);
+numTwo=(30);
+
+console.log(numOne + numTwo);
+console.log(numOne - numTwo);
+console.log(numOne / numTwo);
+console.log(numOne % numTwo);
+console.log(numOne * numTwo);
